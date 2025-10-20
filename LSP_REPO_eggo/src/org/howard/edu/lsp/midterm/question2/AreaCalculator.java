5440040f3dc2f578d3dcb4ffd5d3ab097a33a7c1
jadag <jadag@LAPTOP-S1FTC8E0>
2025-10-20 19:14:46 -0400

    Updated

--- LSP_REPO_eggo/src/org/howard/edu/lsp/midterm/question2/AreaCalculator.java
+++ LSP_REPO_eggo/src/org/howard/edu/lsp/midterm/question2/AreaCalculator.java
@@ -3,32 +3,48 @@ package org.howard.edu.lsp.midterm.question2;
 public class AreaCalculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			System.out.println("Circle area: " + area(3.0));
+            System.out.println("Rectangle area: " + area(5.0, 2.0));
+            System.out.println("Triangle area: " + area(10, 6));
+            System.out.println("Square area: " + area(4)); 
+			
+		} catch (IllegalArgumentException e) {
+			System.err.println("Error: " + e.getMessage());
+		}
 
 	}
 	
 	// Circle area
 	public static double area(double radius) {
-		double circleArea = Math.PI * Math.pow(radius, 2);
-		return circleArea;
+		if (radius <= 0) {
+			throw new IllegalArgumentException("Radius should be greater than 0.");
+		}
+			return Math.PI * Math.pow(radius, 2);
 	}
 
 	// Rectangle area
 	public static double area(double width, double height) {
-		double rectangleArea = width * height;
-		return rectangleArea;
+		if (width <= 0 || height <= 0) {
+            throw new IllegalArgumentException("The width and height values must be greater than 0.");
+        }
+        return width * height;
 	}
 
 	// Triangle (base & height) area
 	public static double area(int base, int height) {
-		double triangleArea = (1/2) * base * height;
-		return triangleArea;
+		if (base <= 0 || height <= 0) {
+            throw new IllegalArgumentException("The base and height values must be greater than 0.");
+        }
+        return 0.5 * base * height;
 	}
 
 	// Square (side length) area
 	public static double area(int side) {
-		double squareArea = Math.pow(side, side);
-		return squareArea;
+		 if (side <= 0) {
+	            throw new IllegalArgumentException("The side length must be greater than 0.");
+	        }
+	        return Math.pow(side, 2);
 	}
 
 }
